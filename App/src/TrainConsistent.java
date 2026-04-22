0995b9ea7803e10e4812264f867c9ca563a0d431
SrilathaSelvam <sri192006@gmail.com>
2026-04-22 11:01:34 +0530

    UC19

--- App/src/TrainConsistent.java
+++ App/src/TrainConsistent.java
@@ -1,17 +1,26 @@
 public class TrainConsistent {
     public static void main(String[] args) {
 
-        System.out.println("=== UC18: Linear Search ===");
+        System.out.println("=== UC19: Binary Search ===");
 
-        String[] bogieIds = {"B1", "B2", "B3", "B4"};
+        String[] bogieIds = {"B1", "B2", "B3", "B4", "B5"};
         String key = "B3";
 
+        int low = 0, high = bogieIds.length - 1;
         boolean found = false;
 
-        for (String id : bogieIds) {
-            if (id.equals(key)) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            int cmp = key.compareTo(bogieIds[mid]);
+
+            if (cmp == 0) {
                 found = true;
                 break;
+            } else if (cmp > 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
 
