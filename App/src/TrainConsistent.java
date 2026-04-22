8fa8e90d997ad375578e37f20c7d5e8605f9eb59
SrilathaSelvam <sri192006@gmail.com>
2026-04-22 10:57:41 +0530

    UC17

--- App/src/TrainConsistent.java
+++ App/src/TrainConsistent.java
@@ -1,24 +1,14 @@
+import java.util.Arrays;
+
 public class TrainConsistent {
     public static void main(String[] args) {
 
-        System.out.println("=== UC16: Bubble Sort ===");
+        System.out.println("=== UC17: Arrays.sort() ===");
 
-        int[] capacities = {72, 40, 60, 20};
+        String[] bogies = {"Sleeper", "AC", "First Class", "Cargo"};
 
-        // Bubble Sort
-        for (int i = 0; i < capacities.length - 1; i++) {
-            for (int j = 0; j < capacities.length - i - 1; j++) {
-                if (capacities[j] > capacities[j + 1]) {
-                    int temp = capacities[j];
-                    capacities[j] = capacities[j + 1];
-                    capacities[j + 1] = temp;
-                }
-            }
-        }
+        Arrays.sort(bogies);
 
-        // Print sorted array
-        for (int c : capacities) {
-            System.out.print(c + " ");
-        }
+        System.out.println("Sorted Bogies: " + Arrays.toString(bogies));
     }
 }
